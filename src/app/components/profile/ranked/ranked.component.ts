36fd64924fc467fe7c6a273a69dbe71a5d780cc8
evenatthaphan <162254547+evenatthaphan@users.noreply.github.com>
2024-03-29 10:16:27 +0700

    hvj

--- src/app/components/profile/ranked/ranked.component.ts
+++ src/app/components/profile/ranked/ranked.component.ts
@@ -1,15 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { ChangeDetectorRef } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { ImagePostRequest } from '../../../model/data_get_res';
-import { Router } from 'express';
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { Constants } from '../../../config/constants';
 import { HttpClient } from '@angular/common/http';
-import { MatToolbar } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-ranked',
@@ -20,111 +18,104 @@ import { MatButtonModule } from '@angular/material/button';
     RouterOutlet,
     CommonModule,
     MatCardModule,
-    MatIconModule,
+    MatIconModule
   ],
   templateUrl: './ranked.component.html',
-  styleUrl: './ranked.component.scss',
+  styleUrl: './ranked.component.scss'
 })
 export class RankedComponent {
 
-  // showImage: boolean = true;
-  data: any;
-  allimage: ImagePostRequest[] = [];
-  userId: number | null = null; 
-  score_current: any;
-  score_old: any;
-  oldScores: any;
-  result: any;
+    // showImage: boolean = true;
+    data: any;
+    allimage: any;
+    userId: number | null = null; 
+    getrank: any;
+    getpoint: any;
+    result: any;
+    
+    constructor(
+      private router: Router,
+      private route: ActivatedRoute,
+      private cdr: ChangeDetectorRef,
+      private constants: Constants,
+      private http: HttpClient
+    ) {}
 
-  constructor(
-    private route: ActivatedRoute,
-    private cdr: ChangeDetectorRef,
-    private constants: Constants,
-    private http: HttpClient
-  ) {}
-
-ngOnInit() {
-  this.route.queryParams.subscribe((params) => {
-      this.route.queryParams.subscribe(params => {
-  try {
-    if (typeof params['data'] === 'string') {
-      this.data = JSON.parse(params['data']);
-    } else {
-      this.data = params['data']; // ใช้ข้อมูลเดิมถ้าไม่ได้รับเป็นสตริง JSON
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+        this.route.queryParams.subscribe(params => {
+    try {
+      if (typeof params['data'] === 'string') {
+        this.data = JSON.parse(params['data']);
+      } else {
+        this.data = params['data']; // ใช้ข้อมูลเดิมถ้าไม่ได้รับเป็นสตริง JSON
+      }
+    } catch (error) {
+      console.error('Error parsing JSON data:', error);
     }
-  } catch (error) {
-    console.error('Error parsing JSON data:', error);
-  }
-});
-    console.log('Received data:', this.data);
-    const firstItem = this.data.length > 0 ? this.data : null;
-    this.userId = firstItem ? firstItem.userID : null;
-
-    this.cdr.detectChanges(); // Force change detection
   });
+      console.log('Received data in profile:', this.data);
+      const firstItem = this.data.length > 0 ? this.data[0] : null;
+      this.userId = firstItem ? firstItem.userID : null;
 
-  this.getallpic();
-  this.getScore_current()
-  this.calculateImageRanks()
-}
+      this.cdr.detectChanges(); // Force change detection
+      this.getallpic();
+    });
 
-getallpic() {
-  const url = `${this.constants.API_ENDPOINT}/show/getimage/${this.data.userID}`;
+    this.getallpic();
+    this.getRanked();
+    this.getPoint();
+  }
 
-  this.http.get(url).subscribe(
-    (response: any) => {
-      console.log('API Response:', response, this.data.userID);
-      this.allimage = response;
-    },
-    (error) => {
-      console.error('API Error:', error);
-    }
-  );
-}
+  getallpic() {
+    const url = `${this.constants.API_ENDPOINT}/show/getimage/${this.data[0].userID}`;
 
-getScore_current() {
-  const url = this.constants.API_ENDPOINT + '/rank/chart';
+    this.http.get(url).subscribe(
+      (response: any) => {
+        console.log('API Response getallpic:', response);
+        this.allimage = response;
+      },
+      (error) => {
+        console.error('API Error:', error);
+      }
+    );
+  }
 
-  this.http.get(url).subscribe(
-    (response: any) => {
-      console.log('API Response Ranking:', response);
-      
-      this.score_current = response.filter((score: { userID: number; }) => score.userID === this.data.userID);
-      console.log('Ranking:', this.score_current);
-    },
-    (error) => {
-      console.error('API Error:', error);
-    }
-  );
-}
+  getRanked(){
+    const urlall = this.constants.API_ENDPOINT + '/show/get/diff';
+    this.http.get(urlall).subscribe(
+      (response: any) => {
+        this.getrank = response;
+        console.log('API Response getRanked:', response);
+      },
+      (error) => {
+        console.error('API Error:', error);
+      }
+    );
+  }
 
-calculateImageRanks() {
-  const url = this.constants.API_ENDPOINT + '/rank/rank';
-  
-  this.http.get(url).subscribe(
-    (response: any) => {
-      console.log('API Response Ranking:', response);
-      
-      const oldScores: { [key: number]: any[] } = {}; // สร้างออบเจกต์เปล่าเพื่อเก็บข้อมูลระดับเก่าโดยใช้ imageID เป็น key
-
-      // วนลูปผ่านคะแนนปัจจุบันทุกคะแนน
-      this.score_current.forEach((currentScore: { imageID: number; }) => {
-        // กรองข้อมูลระดับเก่าที่มี imageID ตรงกับคะแนนปัจจุบัน
-        const filteredScores = response.filter((score: { imageID: number; }) => score.imageID === currentScore.imageID);
+  getPoint() {
+    const urls = this.constants.API_ENDPOINT + '/rank/chart';
+    this.http.get(urls).subscribe(
+      (response: any) => {
+        this.getpoint = response;
+        console.log('API Response getPoint:', response);
+        let foundItem = [];
         
-        // เก็บข้อมูลระดับเก่าลงในออบเจกต์โดยใช้ imageID เป็น key
-        oldScores[currentScore.imageID] = filteredScores;
-        
-        console.log('Ranking for score_current with imageID', currentScore.imageID, ':', filteredScores);
-      });
-      
-      console.log('Old Scores:', oldScores);
-      
-      // สามารถใช้ oldScores ในการนำข้อมูลระดับเก่าไปใช้งานต่อได้ตามต้องการ
-    },
-    (error) => {
-      console.error('API Error:', error);
-    }
-  );
-}  
+        for (let i = 0; i < this.allimage.length; i++) {
+          let currentItem = this.getpoint.find((item: { imageID: any; }) => item.imageID === this.allimage[i].imageID);
+          if (currentItem) {
+            foundItem.push(currentItem);
+          } else {
+            console.log('No item found with imageID:', this.allimage[i].imageID);
+          }
+        }
+        console.log('Item found:', foundItem);
+        this.result = foundItem;
+      },
+      (error) => {
+        console.error('API Error:', error);
+      }
+    );
+  }
 }
